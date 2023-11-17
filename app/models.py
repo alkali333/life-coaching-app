25bfdda35d7a5aee9c8768e3453bc49ca5807559
alkali333 <jake@alkalimedia.co.uk>
2023-11-17 11:35:12 +0000

    Hid exercises from new users NOTE: DB connection is not working on Heroku, fix before deploying there

--- app/models.py
+++ app/models.py
@@ -67,14 +67,6 @@ pool_size = 10  # Maximum number of connections
 pool_timeout = 10  # Maximum time to wait for a connection
 pool_recycle = 3600  # Maximum age of connections in seconds
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    pool_size=pool_size,
-    pool_timeout=pool_timeout,
-    pool_recycle=pool_recycle,
-    pool_pre_ping=True,
-)
-
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
